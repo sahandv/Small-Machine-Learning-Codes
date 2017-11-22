2d6c72e41ff584590aa4e04db8b1b1cb50d6c1b1
sahand v <sahand.vahidnia@yahoo.com>
2017-11-22 17:06:18 +0300

    nothing yet, just figuring out the method

--- DecisionTree.py
+++ DecisionTree.py
@@ -8,17 +8,20 @@ Created on Wed Nov 22 12:15:14 2017
 
 import pandas as pd
 import numpy as np
+import time
 import random
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
 
-test_file_address = r'optdigits.tes'
-train_file_address = r'optdigits.tra'
+TestFilePath = r'data/optdigits.tes'
+TrainFilePath = r'data/optdigits.tra'
 
-test_data_raw = pd.read_csv(test_file_address, sep=',')
-train_data_raw = pd.read_csv(train_file_address, sep=',')
+TestDataRaw = pd.read_csv(TestFilePath, sep=',', header=None)
+TrainDataRaw = pd.read_csv(TrainFilePath, sep=',', header=None)
 
-
-
+#Pre processing data
+#get labels
+TestLabels = TestDataRaw[TestDataRaw.shape[1]-1]
+TrainLabels = TrainDataRaw[TrainDataRaw.shape[1]-1]
 
