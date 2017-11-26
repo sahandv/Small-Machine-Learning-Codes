bbc38f4a6b3ae560130924470f248da31b6d6791
sahand v <sahand.vahidnia@yahoo.com>
2017-11-26 18:27:26 +0300

    nothing yet
    but the shoreline is visible now!

--- DecisionTree.py
+++ DecisionTree.py
@@ -13,11 +13,85 @@ import random
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
+Nodes = {}
 
 def entropy(df_list):
     result = -np.sum(df_list*np.log2(df_list)) 
     return(result)
     
+class treeNode():
+    def __init__(self, is_leaf, attr_index, attr_table, parent, height):
+        self.is_leaf = is_leaf
+        self.attr_split_points = None
+        self.attr_index = attr_index
+        self.attr_table = attr_table
+        self.parent = parent
+        self.right = None
+        self.left = None
+        self.height = height
+
+def nodeFinder(TrainData):
+    TotalEntropy = []
+    TotalEntropy = pd.DataFrame(TotalEntropy)
+    TotalGain = []
+    TotalGain = pd.DataFrame(TotalGain)
+    AttributeTable_N = {}
+    AttributeTable_T = {}
+    #For each column/attribute (probably 64 in opdigits data):
+    for i in range(0,TrainData.shape[1]): 
+        TempData_column_and_label =  TrainDataRaw[[i,TrainDataRaw.shape[1]-1]]
+        AttributeTable = []
+        AttributeTable = pd.DataFrame(AttributeTable)
+        AttributeTable_col_sums = []
+        AttributeTable_col_sums = pd.DataFrame(AttributeTable_col_sums)
+        AttributeTable_probabilities = []
+        AttributeTable_probabilities = pd.DataFrame(AttributeTable_probabilities)
+        AttributeTable_class_probability = []
+        AttributeTable_class_probability = pd.DataFrame(AttributeTable_class_probability)
+        AttributeTable_class_entropy = []
+        AttributeTable_class_entropy = pd.DataFrame(AttributeTable_class_entropy)
+        #    For each possible value of a column (probably between 0 and 16 in opdigits data):
+        for j in range(0,MaxAttributeValue+1):
+            
+        #           AttributeTable.T shape
+        #           0 1 2 3 4 5 6 7 8 9 (classes)
+        #       0 \  TempFrequency = [0]  \
+        #       1 \  TempFrequency = [1]  \
+        #       2 \  TempFrequency = [2]  \
+        #       3 \  TempFrequency = [3]  \
+        #                 ......
+        #      15 \  TempFrequency = [15] \
+        #      16 \  TempFrequency = [16] \
+             
+             ValuesInColumn = TempData_column_and_label.loc[TempData_column_and_label[i]==j] #Rows with value j in column i
+         #        Find frequency of classes for the value j of column i (will give a 10 element array):
+             TempClassFrequency = []
+             for k in range(0,10):
+                 TempClassFrequency.append(len(ValuesInColumn.loc[ValuesInColumn[TrainDataRaw.shape[1]-1]==k]))
+             del k
+        
+             AttributeTable[j] = TempClassFrequency
+             AttributeTable_col_sums[j] = [np.sum(TempClassFrequency)]
+             AttributeTable_probabilities[j] = [AttributeTable_col_sums[j]/TrainDataSize] #Calculate probability of each value of attribute
+        #    Find probability of each class in each value of each column
+             AttributeTable_class_probability[j] = TempClassFrequency/np.sum(TempClassFrequency)
+        #    Find the entropy of 16 values for each class
+             AttributeTable_class_entropy[j] = [entropy(AttributeTable_class_probability[j])]
+        
+        TotalEntropy[i] = [np.sum(np.sum(AttributeTable_probabilities*AttributeTable_class_entropy))]
+        TotalGain[i] = [Entropy_N-TotalEntropy[i]]
+        AttributeTable_T[i] = AttributeTable.T
+        AttributeTable_N[i] = AttributeTable
+    
+    #End of loop
+
+    TotalGain = np.max(TotalGain)
+    TotalGain = pd.DataFrame(TotalGain)
+    TotalGain['gain'] = TotalGain[0]
+    TotalGain = TotalGain.drop(0,axis=1)
+    MaxGain_index = TotalGain['gain'].argmax() #This is a decision node
+    
+    return(MaxGain_index,AttributeTable_N,AttributeTable_T,TotalEntropy)
 
 # =============================================================================
 # INITIALIZATION
@@ -31,12 +105,12 @@ TrainDataRaw = pd.read_csv(TrainFilePath, sep=',', header=None)
 del TestFilePath
 del TrainFilePath
 
-
 #Pre processing data
 #Get labels from far right elements of dataframe
 TestLabels = TestDataRaw[TestDataRaw.shape[1]-1]
 TrainLabels = TrainDataRaw[TrainDataRaw.shape[1]-1]
 TrainData = TrainDataRaw.drop(TrainDataRaw.shape[1]-1,axis=1)
+TrainData_temp = TrainData
 TestData = TestDataRaw.drop(TestDataRaw.shape[1]-1,axis=1)
 TrainDataSize = TrainData.shape[0]
 TestDataSize = TestData.shape[0]
@@ -70,71 +144,69 @@ MinAttributeValue = 0
 # =============================================================================
 # INFORMATION GAIN
 # =============================================================================
-TotalEntropy = []
-TotalEntropy = pd.DataFrame(TotalEntropy)
-TotalGain = []
-TotalGain = pd.DataFrame(TotalGain)
-#For each column/attribute (probably 64 in opdigits data):
-for i in range(0,TrainData.shape[1]): 
-    TempData_column_and_label =  TrainDataRaw[[i,TrainDataRaw.shape[1]-1]]
-    AttributeTable = []
-    AttributeTable = pd.DataFrame(AttributeTable)
-    AttributeTable_col_sums = []
-    AttributeTable_col_sums = pd.DataFrame(AttributeTable_col_sums)
-    AttributeTable_probabilities = []
-    AttributeTable_probabilities = pd.DataFrame(AttributeTable_probabilities)
-    AttributeTable_class_probability = []
-    AttributeTable_class_probability = pd.DataFrame(AttributeTable_class_probability)
-    AttributeTable_class_entropy = []
-    AttributeTable_class_entropy = pd.DataFrame(AttributeTable_class_entropy)
-    #    For each possible value of a column (probably between 0 and 16 in opdigits data):
-    for j in range(0,MaxAttributeValue+1):
-        
-    #     AttributeTable shape (in reverse)
-    #           0 1 2 3 4 5 6 7 8 9 (classes)
-    #       0 \  TempFrequency = [0]  \
-    #       1 \  TempFrequency = [1]  \
-    #       2 \  TempFrequency = [2]  \
-    #       3 \  TempFrequency = [3]  \
-    #                 ......
-    #      15 \  TempFrequency = [15] \
-    #      16 \  TempFrequency = [16] \
-         
-         ValuesInColumn = TempData_column_and_label.loc[TempData_column_and_label[i]==j] #Rows with value j in column i
-     #        Find frequency of classes for the value j of column i (will give a 10 element array):
-         TempClassFrequency = []
-         for k in range(0,10):
-             TempClassFrequency.append(len(ValuesInColumn.loc[ValuesInColumn[TrainDataRaw.shape[1]-1]==k]))
-         del k
-    
-         AttributeTable[j] = TempClassFrequency
-         AttributeTable_col_sums[j] = [np.sum(TempClassFrequency)]
-         AttributeTable_probabilities[j] = [AttributeTable_col_sums[j]/TrainDataSize] #Calculate probability of each value of attribute
-    #    Find probability of each class in each value of each column
-         AttributeTable_class_probability[j] = TempClassFrequency/np.sum(TempClassFrequency)
-    #    Find the entropy of 16 values for each class
-         AttributeTable_class_entropy[j] = [entropy(AttributeTable_class_probability[j])]
 
-    TotalEntropy[i] = [np.sum(np.sum(AttributeTable_probabilities*AttributeTable_class_entropy))]
-    TotalGain[i] = [Entropy_N-TotalEntropy[i]]
 
-TotalGain = np.max(TotalGain)
-TotalGain = pd.DataFrame(TotalGain)
-TotalGain['gain'] = TotalGain[0]
-TotalGain = TotalGain.drop(0,axis=1)
+MaxGain_index,AttributeTable_N,AttributeTable_T,TotalEntropy = nodeFinder(TrainData)
 
 #Now we have the best break node
+#If there is no pure branch, further split is required.
+AttributeTable_other_method = TrainDataRaw.pivot_table(0,index=TrainData.shape[1], columns=MaxGain_index,aggfunc='count')
 
+all_is_leaf = False
 
-class treeNode():
-    def __init__(self, is_leaf, classification, attr_split_index, attr_split_value, parent, upper_child, lower_child, height):
-        self.is_leaf = True
-        self.classification = None
-        self.attr_split = None
-        self.attr_split_index = None
-        self.attr_split_value = None
-        self.parent = parent
-        self.right = None
-        self.left = None
-        self.height = None
+for epoch in range(0,1): 
+
+    Nodes[epoch] = treeNode(False,MaxGain_index,AttributeTable_T[MaxGain_index],None,0)
+    if all_is_leaf:
+        TrainData_temp = TrainData_temp.drop(MaxGain_index,axis=1)
+    
+    for i in range(0,MaxAttributeValue+1): 
+        TrainData_split = TrainData.loc[TrainData[MaxGain_index]==0]
         
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
